8dae854d3d38b7588328666a3d0290f5513f3b11
Karan Nair <nairkaran623@gmail.com>
2025-02-25 10:52:24 +0530

    atoi

---
+++ Karan/Strings/String to Integer (atoi).py
@@ -0,0 +1,24 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        if not s:
+            return 0
+        sign = 1
+        if s[0] == '-':
+            sign = -1
+            s = s[1:] 
+        elif s[0] == '+':
+            s = s[1:]
+        result = 0
+        for char in s:
+            if not char.isdigit():
+                break
+            result = result * 10 + int(char)
+        result *= sign
+        int_min = -2**31
+        int_max = 2**31 - 1
+        if result < int_min:
+            return int_min
+        if result > int_max:
+            return int_max
+        return result
