f390054474211d0ccbc156be807edeb95f755564
Karan Nair <nairkaran623@gmail.com>
2025-03-18 17:51:50 +0530

    longest subarr

---
+++ Karan/Arrays/Longest Nice Subarray.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+int longestNiceSubarray(vector<int>& nums) {
+int left = 0, bitmask = 0, max_length = 0;
+for (int right = 0; right < nums.size(); right++) {
+while ((bitmask & nums[right]) != 0) {
+bitmask ^= nums[left];
+left++;
+}
+bitmask |= nums[right];
+max_length = max(max_length, right - left + 1);
+}
+return max_length;
+}
+};
