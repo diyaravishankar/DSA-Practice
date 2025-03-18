6d6022106d33a3787a2ae887a038c8c109c20da0
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-03-18 17:53:54 +0530

    Update Container With Most Water.py

--- Karan/Greedy/Container With Most Water.py
+++ Karan/Greedy/Container With Most Water.py
@@ -6,7 +6,7 @@ class Solution:
             current_area = (right - left) * min(height[left], height[right])
             max_area = max(max_area, current_area)
             if height[left] < height[right]:
-                left += 2
+                left += 1
             else:
                 right -= 1
         return max_area
