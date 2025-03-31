d7be6766fa457d90e0bcc17ee33b91316c978659
Karan Nair <nairkaran623@gmail.com>
2025-03-31 20:37:15 +0530

    marbles

---
+++ Karan/Greedy/Put Marbles in Bags.py
@@ -0,0 +1,7 @@
+class Solution:
+ def putMarbles(self, weights: list[int], k: int) -> int:
+  if k == 1: return 0
+  n = len(weights)
+  pair_sums = [weights[i] + weights[i + 1] for i in range(n - 1)]
+  pair_sums.sort()
+  return sum(pair_sums[-(k-1):]) - sum(pair_sums[:k-1])
