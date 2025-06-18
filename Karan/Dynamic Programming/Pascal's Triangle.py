62af39200c6a2b30f3b88dddb4ba7ea483fa0aaf
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-06-18 21:37:32 +0530

    p triangle

---
+++ Karan/Dynamic Programming/Pascal's Triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+ def generate(self, numRows: int) -> List[List[int]]:
+  res = [[1]]
+  for i in range(1, numRows):
+   row = [1]
+   for j in range(1, i):
+    row.append(res[i - 1][j - 1] + res[i - 1][j])
+   row.append(1)
+   res.append(row)
+  return res
