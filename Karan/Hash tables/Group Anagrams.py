76b4407d0ec9d6ec34e325a4cc1cc4bf0a26b8ae
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-06-22 14:45:31 +0530

    Group Anagrams

---
+++ Karan/Hash tables/Group Anagrams.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+ def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+  groups = defaultdict(list)
+  for word in strs:
+   key = ''.join(sorted(word))
+   groups[key].append(word)
+  return list(groups.values())
