b709aac3833171e1c15eabe6e1abd0a188c94b6c
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-06-24 22:06:14 +0530

    for loop iter

--- Karan/Arrays/Find All K-Distant Indices in an Array.py
+++ Karan/Arrays/Find All K-Distant Indices in an Array.py
@@ -2,4 +2,6 @@ class Solution:
  def findKDistantIndices(self, nums, key, k):
   key_indices = [i for i, num in enumerate(nums) if num == key]
   result = set()
-  
+  for idx in key_indices:
+   for i in range(max(0, idx - k), min(len(nums), idx + k + 1)):
+    
