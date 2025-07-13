45106d38075134298f319d3966bb183ceb725196
Diya Ravishankar <123184100+diyaravishankar@users.noreply.github.com>
2025-07-13 20:06:13 +0530

    Time: 9 ms (98.36%) | Memory: 65.7 MB (21.05%) - LeetSync

--- Diya/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ Diya/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,21 +1,31 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Thread t1 = new Thread(()->Arrays.sort(players));
-        Thread t2 = new Thread(()->Arrays.sort(trainers));
-        t1.start();
-        t2.start();
-        try{
-            t1.join();
-            t2.join();
-        }catch(Exception e){}
-        int cnt=0,i=0,j=0;
-        while(i<players.length && j<trainers.length){
-            if(players[i]<=trainers[j]){
-                cnt++;
+        CountDownLatch latch = new CountDownLatch(2);
+
+        Thread.startVirtualThread(() -> {
+            Arrays.sort(players);
+            latch.countDown();
+        });
+        Thread.startVirtualThread(() -> {
+            Arrays.sort(trainers);
+            latch.countDown();
+        });
+
+        try {
+            latch.await();
+        } catch (Exception e) {
+
+        }
+
+
+        int i = 0, j = 0;
+        int n = players.length, m = trainers.length;
+        while (i < n && j < m) {
+            if (players[i] <= trainers[j]) {
                 i++;
             }
             j++;
         }
-        return cnt;
+        return i;
     }
 }
