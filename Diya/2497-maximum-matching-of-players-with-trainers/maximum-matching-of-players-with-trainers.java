d1fac790a3115e9b79c969f3f83894dabd2ac61b
Diya Ravishankar <123184100+diyaravishankar@users.noreply.github.com>
2025-07-13 20:05:32 +0530

    Time: 6 ms (100.00%) | Memory: 64.7 MB (34.64%) - LeetSync

--- Diya/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ Diya/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,27 +1,61 @@
 class Solution {
-    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+    // public int minMatchIdx(int s, int e, int[] trainers, int target) {
+    //     int ans = trainers.length;
+    //     while (s <= e) {
+    //         int m = s + (e - s) / 2;
+    //         if (trainers[m] >= target) {
+    //             ans = m;
+    //             e = m - 1;
+    //         } else {
+    //             s = m + 1;
+    //         }
+    //     }
+    //     return ans;
+    // }
 
-        Thread t1 = new Thread(()->Arrays.sort(players));
-        Thread t2 = new Thread(()->Arrays.sort(trainers));
+    // public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+    //     Arrays.sort(players);
+    //     Arrays.sort(trainers);
+    //     int n = trainers.length;
+    //     int s = 0;
+    //     int e = n - 1;
+    //     int maxMatch = 0;
+    //     for (int p : players) {
+    //         int idxMatchedWithCurrPlayer = minMatchIdx(s, e, trainers, p);
+    //         if (idxMatchedWithCurrPlayer == n) {
+    //             break;
+    //         }
+    //         s = idxMatchedWithCurrPlayer + 1;
+    //         maxMatch++;
+    //     }
+    //     return maxMatch;
+    // }
+
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        int m = players.length;
+        int n = trainers.length;
+
+        Thread t1 = new Thread(() -> Arrays.sort(players));
+        Thread t2 = new Thread(() -> Arrays.sort(trainers));
         t1.start();
         t2.start();
-        try{
+        try {
             t1.join();
             t2.join();
-        }catch(Exception e){}
-
-        //Arrays.sort(players);
-        //Arrays.sort(trainers);
-
-        int matches = 0;
-        int playPtr = 0, trainPtr = 0;
-        while(trainPtr < trainers.length && playPtr < players.length) {
-            if ( players[playPtr] <= trainers[trainPtr]) {
-                playPtr++; trainPtr++; matches++;
-            }
-            else trainPtr++;   
+        } catch (Exception e) {
         }
 
-        return matches;
+        int left = 0;
+        int right = 0;
+        int count = 0;
+
+        while (left < m && right < n) {
+            if (trainers[right] >= players[left]) {
+                count++;
+                left++;
+            }
+            right++;
+        }
+        return count;
     }
 }
