2c4c3d81ac4908803d8d997c7bd2c0ece0485dda
Diya Ravishankar <123184100+diyaravishankar@users.noreply.github.com>
2025-07-13 20:06:32 +0530

    Time: 51 ms (5.41%) | Memory: 56.6 MB (99.75%) - LeetSync

--- Diya/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ Diya/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,31 +1,41 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        CountDownLatch latch = new CountDownLatch(2);
-
-        Thread.startVirtualThread(() -> {
-            Arrays.sort(players);
-            latch.countDown();
-        });
-        Thread.startVirtualThread(() -> {
-            Arrays.sort(trainers);
-            latch.countDown();
-        });
-
-        try {
-            latch.await();
-        } catch (Exception e) {
-
-        }
-
-
-        int i = 0, j = 0;
-        int n = players.length, m = trainers.length;
-        while (i < n && j < m) {
-            if (players[i] <= trainers[j]) {
-                i++;
+        sort(players,0,players.length-1);
+        sort(trainers,0,trainers.length-1);
+        int matchings=0;
+        int i=0,j=0;
+        while(i<players.length && j<trainers.length){
+            if(players[i]<=trainers[j]){
+                matchings++;
+                j++;
             }
-            j++;
+            i++;
         }
-        return i;
+        return matchings;
+    }
+    void sort(int[] nums,int s,int e){
+        if(s==e){
+            return;
+        }
+        int m=s+(e-s)/2;
+        sort(nums,s,m);
+        sort(nums,m+1,e);
+        conquer(nums,s,m,e);
+    }
+    void conquer(int[] nums,int s,int m,int e){
+        int[] mix=new int[e-s+1];
+        int i=s,j=m+1,k=0;
+        while(i<=m && j<=e){
+            mix[k++]=nums[i]>nums[j]?nums[i++]:nums[j++];
+        }
+        while(i<=m){
+            mix[k++]=nums[i++];
+        }
+        while(j<=e){
+            mix[k++]=nums[j++];
+        }
+        for(int a=0;a<mix.length;a++){
+            nums[a+s]=mix[a];
+        }
     }
 }
