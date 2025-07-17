9cdd83ed18d41726b8fb3d8879c71bfd36218a80
Diya Ravishankar <123184100+diyaravishankar@users.noreply.github.com>
2025-07-17 10:42:07 +0530

    Time: 37 ms (70.10%) | Memory: 50.1 MB (88.66%) - LeetSync

--- Diya/3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
+++ Diya/3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,27 +1,17 @@
 class Solution {
-    public int maximumLength1(int[] A, int k1) {
-        int res = 0, k = k1, dp[][] = new int[k][k];
-        for (int a : A) {
-            for (int b = 0; b < k; b++) {
-                //dp[a%k][b] - dp[b][a%k]
-                //same reminder can be only extended with same reminder numbers [b]
-                //And we have to maintain subsequence for different reminders from starting a%k
-                dp[b][a % k] = dp[a % k][b] + 1;
-                res = Math.max(res, dp[b][a % k]);
+    public int maximumLength(int[] nums, int k) {
+        int n = nums.length, ans = 1;
+        int[][] f = new int[n][k];
+        for(int[] x : f) {
+            Arrays.fill(x, 1);
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                int mod = (nums[i] + nums[j]) % k;
+                f[i][mod] = f[j][mod] + 1;
+                ans = Math.max(ans, f[i][mod]);
             }
         }
-        return res;
-    }
-
-        public int maximumLength(int[] A, int k) {
-        int res = 0;
-        for (int v = 0; v < k; v++) {
-            int[] dp = new int[k];
-            for (int a : A) {
-                dp[a % k] = dp[(v + k - a % k) % k] + 1;
-                res = Math.max(res, dp[a % k]);
-            }
-        }
-        return res;
+        return ans;
     }
 }
