49e11ac525f66d7d029e9f1b73e182dc29f0a3d4
Diya Ravishankar <123184100+diyaravishankar@users.noreply.github.com>
2025-08-03 12:54:07 +0530

    Time: 3 ms (74.68%) | Memory: 97.5 MB (72.15%) - LeetSync

--- Diya/2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.java
+++ Diya/2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.java
@@ -1,15 +1,33 @@
 class Solution {
-    public int maxTotalFruits(int[][] f, int pos, int k) {
-    int l = 0, sum = 0, max_sum = 0;
-    while (l < f.length && f[l][0] < pos - k)
-        ++l;
-    for (int r = l; r < f.length && f[r][0] <= pos + k; ++r) {
-        sum += f[r][1];
-        while(Math.min(pos - 2 * f[l][0] + f[r][0], 2 * f[r][0] - f[l][0] - pos) > k)
-            sum -= f[l++][1];
-        max_sum = Math.max(max_sum, sum);
+
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int left = 0;
+        int right = 0;
+        int n = fruits.length;
+        int sum = 0;
+        int ans = 0;
+        // each time fix the right boundary of the window
+        while (right < n) {
+            sum += fruits[right][1];
+            // move left boundary
+            while (left <= right && step(fruits, startPos, left, right) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+            ans = Math.max(ans, sum);
+            right++;
+        }
+        return ans;
     }
-    return max_sum;
-}
-    
+
+    public int step(int[][] fruits, int startPos, int left, int right) {
+        return (
+            Math.min(
+                Math.abs(startPos - fruits[right][0]),
+                Math.abs(startPos - fruits[left][0])
+            ) +
+            fruits[right][0] -
+            fruits[left][0]
+        );
+    }
 }
