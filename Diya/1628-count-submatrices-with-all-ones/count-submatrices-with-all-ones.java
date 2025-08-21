a80f0c380da2062e362e24c41b4b52f800429be1
Diya Ravishankar <123184100+diyaravishankar@users.noreply.github.com>
2025-08-21 20:40:16 +0530

    Time: 5 ms (97.95%) | Memory: 45.3 MB (83.56%) - LeetSync

--- Diya/1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
+++ Diya/1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -1,28 +1,82 @@
 class Solution {
-
     public int numSubmat(int[][] mat) {
-        int n = mat[0].length;
-        int[] heights = new int[n];
-        int res = 0;
-        for (int[] row : mat) {
-            for (int i = 0; i < n; i++) {
-                heights[i] = row[i] == 0 ? 0 : heights[i] + 1;
-            }
-            Stack<int[]> stack = new Stack<>();
-            stack.push(new int[] { -1, 0, -1 });
-            for (int i = 0; i < n; i++) {
-                int h = heights[i];
-                while (stack.peek()[2] >= h) {
-                    stack.pop();
+
+        int count = 0;
+        int[][] height = new int[mat.length][mat[0].length];
+        for(int i = 0;i<mat[0].length;i++){
+            height[0][i] = mat[0][i];
+        }
+
+        for(int i = 1;i<mat.length;i++){
+            for(int j = 0;j<mat[0].length;j++){
+                if(mat[i][j] == 1){
+                    height[i][j] = 1 + height[i-1][j];
                 }
-                int[] top = stack.peek();
-                int j = top[0];
-                int prev = top[1];
-                int cur = prev + (i - j) * h;
-                stack.push(new int[] { i, cur, h });
-                res += cur;
+                
             }
         }
-        return res;
+       for(int i=0;i<height.length;i++){
+        count += fcount(height[i]);
+       }
+       return count;
+
+    }
+    int fcount(int[] row){
+         int col = row.length;
+         int ans = 0;
+         for(int i = 0;i<col;i++){
+            if(row[i]!=0){
+            int minh = row[i];
+            for(int j = i;j>=0;j--){
+              minh = Math.min(minh,row[j]);
+              if(minh == 0){
+                break;
+              }
+              ans+= minh;
+            }
+            }    
+         }
+         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int numSubmat(int[][] mat) {
+//         int row = mat.length;
+//         int col = mat[0].length;
+//         int count = 0;
+//         for(int i = 0;i<row;i++){
+//             for(int j=0;j<col;j++){
+              
+//               for(int k = i;k<row;k++){
+//                 for(int l = j;l<col;l++ ){
+//                      if(all1(mat,i,j,k,l)){
+//                         count++;
+//                      }
+//                 }
+//               }
+//             }
+//         }
+//         return count;
+//     }
+//     boolean all1(int[][] mat,int row1,int col1,int row2,int col2){
+//         for(int i = row1;i<=row2;i++){
+//             for(int j = col1;j<=col2;j++){
+//                 if(mat[i][j] == 0){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
