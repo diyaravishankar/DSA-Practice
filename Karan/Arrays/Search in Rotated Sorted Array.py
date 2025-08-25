561b9f94cebdb5d3e2b597244a55482b6bd3718e
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-08-25 23:02:53 +0530

    rot sort arr

---
+++ Karan/Arrays/Search in Rotated Sorted Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self,n,t):
+        l,r=0,len(n)-1
+        while l<=r:
+            m=(l+r)//2
+            if n[m]==t:return m
+            if n[l]<=n[m]:
+                if n[l]<=t<n[m]:r=m-1
+                else:l=m+1
+            else:
+                if n[m]<t<=n[r]:l=m+1
+                else:r=m-1
+        return -1
