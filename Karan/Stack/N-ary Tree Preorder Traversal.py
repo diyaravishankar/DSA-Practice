072b32d5e6bbe7e1787c2f2d08989bf4c6dff1de
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-08-26 00:39:42 +0530

    n arr

---
+++ Karan/Stack/N-ary Tree Preorder Traversal.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+class Solution:
+    def preorder(self, root):
+        if not root:
+            return []
+        res=[root.val]
+        for child in root.children:
+            res.extend(self.preorder(child))
+        return res
