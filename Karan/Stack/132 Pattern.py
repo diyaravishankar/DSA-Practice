440fb74fa94e892b93f571bddcb4c415de346ab7
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-08-26 00:37:21 +0530

    123

---
+++ Karan/Stack/132 Pattern.py
@@ -0,0 +1,11 @@
+class Solution:
+    def find132pattern(self, nums):
+        stack=[]
+        s3=float('-inf')
+        for num in reversed(nums):
+            if num<s3:
+                return True
+            while stack and num>stack[-1]:
+                s3=stack.pop()
+            stack.append(num)
+        return False
