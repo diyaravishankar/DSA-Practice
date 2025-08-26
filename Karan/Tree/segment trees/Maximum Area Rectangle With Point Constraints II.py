e36ccbb28f9b6dca06857142be81d3a8094619e9
Karan Nair <128114868+skysthelimit007@users.noreply.github.com>
2025-08-26 23:43:51 +0530

    max rect const 2

---
+++ Karan/Tree/segment trees/Maximum Area Rectangle With Point Constraints II.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxRectangleArea(self, xCoord: List[int], yCoord: List[int]) -> int:
+        points = list(zip(xCoord, yCoord))
+        points.sort()
+        yAxis = sorted(set(yCoord))
+        yPos = {y: i for i,y in enumerate(yAxis)}
+        yMax = {y: -inf for y in yAxis}
+        maxTree = SegTree(len(yAxis))
+        prevX, prevY = points[0]
+        res = -1
+        for x,y in points[1:]:
+            if prevX == x and yMax[prevY] == yMax[y] \
+                and yMax[y] > maxTree.query(yPos[prevY], yPos[y]):
+                res = max(res, (y-prevY) * (x - yMax[y]))
+            yMax[prevY] = prevX
+            maxTree.update(yPos[prevY], prevX)
+            prevX, prevY = x, y
+        return res
+class SegTree:
+    def __init__(self, n: int):
+        self.n = 1<<n.bit_length()
+        self.tree = [-inf] * (self.n<<1)
+    def update(self, idx: int, val: int):
+        idx += self.n
+        while idx:
+            self.tree[idx] = val
+            idx >>= 1
+    def query(self, l: int, r: int) -> int:
+        res = -inf
+        l += self.n
+        r += self.n
+        while r-l > 1:
+            if not l&1:
+                res = max(res, self.tree[l+1])
+            if r&1:
+                res = max(res, self.tree[r-1])
+            l >>= 1
+            r >>= 1
+        return res
